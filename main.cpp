8df4da40fd2a782f66dacfff85023e689fda461e
Siarhei Karnialiuk <siarhei.karnialiuk.ext@ericsson.com>
2024-03-08 11:40:36 +0100

    Creating the project and his folder structure

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    std::cout << "Hello" << std::endl;
 
     return a.exec();
 }
